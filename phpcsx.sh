6d57b345e592f6e19384c195117f3633c23223f6
Naser Sobhan <nasersobhan@outlook.com>
2019-07-20 10:06:57 +0430

    Update phpcsx.sh

--- phpcsx.sh
+++ phpcsx.sh
@@ -8,17 +8,21 @@ NCL='\033[0m'           # No Color
 
 phpcsxx(){
         #phpcs '--standard=Drupal' $entry
-        cmd="phpcs --standard=Drupal --extensions=php,module,inc,install,test,profile,theme,css,info,txt,md $entry"
-        #echo -e "$${NCL}"
-        eval $cmd
-        Extport=$?
-        if [[ "$Extport" == 0 ]]
+        if [[ $entry == *.php ]] || [[ $entry == *.module ]]
         then
-                echo -e "${GRE}No Error: phpcs --standard=Drupal $entry${NCL}"  
+                cmd="phpcs --standard=Drupal --extensions=php,module,inc,install,test,profile,theme,info,txt,md $entry"
+                eval $cmd
+                Extport=$?
+                if [[ "$Extport" == 0 ]]
+                then
+                        echo -e "${GRE}No Error: phpcs --standard=Drupal $entry${NCL}"  
+                else
+                        echo -e "${RED}---------------------------------------------------------------------------${NCL}" 
+                        echo -e "${NCL}^ Please Fix above ${RED}Errors${NCL}: $entry ^" 
+                        echo -e "${RED}---------------------------------------------------------------------------${NCL}" 
+                fi
         else
-                echo -e "${RED}---------------------------------------------------------------------------${NCL}" 
-                echo -e "${NCL}^ Please Fix above ${RED}Errors${NCL}: $entry ^" 
-                echo -e "${RED}---------------------------------------------------------------------------${NCL}" 
+                echo -e "${NCL}^ Skipping: ${RED}$entry${NCL} ^"  
         fi
 }
 walk() {
@@ -28,8 +32,6 @@ walk() {
         # If the entry is a file do some operations
         for entry in "$1"/*; do [[ -f "$entry" ]] && phpcsxx "$entry"; done
         
-        #for entry in "$1"/*; do phpcsxx "$entry" ; done
-        
         # If the entry is a directory call walk() == create recursion
         for entry in "$1"/*; do [[ -d "$entry" ]] && walk "$entry" $((indent+4)); done
 }
@@ -37,6 +39,6 @@ walk() {
 # If the path is empty use the current, otherwise convert relative to absolute; Exec walk()
 [[ -z "${1}" ]] && ABS_PATH="${PWD}" || cd "${1}" && ABS_PATH="${PWD}"
 walk "${ABS_PATH}"   
-cmdx="phpcs --extensions=php,module,inc,install,test,profile,theme,css,info,txt,md --standard=Drupal --report=summary ${ABS_PATH}" 
+cmdx="phpcs --extensions=php,module --standard=Drupal --report=summary ${ABS_PATH}" 
 eval $cmdx
 echo      
